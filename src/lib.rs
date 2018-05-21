d201453737728c61ec8cf16615c092729742fd1b
GONI Guillaume <gdn3@live.fr>
2018-05-21 21:49:49 +0200

    cargo + smart pointers (start)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculation more conveniont.
+
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(unused_imports)]
@@ -11,6 +16,7 @@ mod errors;
 mod generics_traits_lifetimes;
 pub mod tests;
 mod functional;
+mod smart_pointers;
 
 mod template;
 mod divers;
@@ -66,11 +72,46 @@ pub fn run() {
     // ------------------------------
     // Functional programming : Closures, Iterators
     {
-        functional::run();
+        // functional::run();
+    }
+    // ------------------------------
+    // Cargo
+    {
+        // See Cargo.toml and this file
+        // Example at the bottom of this file and at the top
+        // Compile the docs and open the browser : `$ cargo doc --open`
+        // Examples are tested when executing : `cargo test`
+        //
+        // `pub use::submodule::subsubmodule::elmt` ...
+        //  |=> create a simple interface for your crate's users
+
+        // ------------------------------
+        // Workspaces
+        {
+            // See workspaces folder
+        }
+    }
+    // ------------------------------
+    // Smart pointers : heap (Box<T>), Deref, Drop, Reference counters, ...
+    {
+        smart_pointers::run();
     }
     // ------------------------------
     // Template
     {
         template::run();
     }
-} 
+}
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let five = 5;
+///
+/// assert_eq!(6, rust_recap::add_one(5));
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
