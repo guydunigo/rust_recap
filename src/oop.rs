f59ab4e66c8b42888d0750618da4ab8ae954bb03
GONI Guillaume <gdn3@live.fr>
2018-06-01 09:50:30 +0200

    Finished polimorphism example

--- src/oop.rs
+++ src/oop.rs
@@ -61,23 +61,20 @@ impl Post {
         }
     }
     fn content(&self) -> &str {
-        match self.state {
-            Some(state) => state.content(&self.content),
-            None => ""
-        }
+        self.state.as_ref().unwrap().content(&self.content)
     }
     fn add_text(&mut self, text_to_add: &str) {
-        if let Some(state) = self.state {
-            state.add_text(self.content, text_to_add);
+        if let Some(state) = &self.state {
+            state.add_text(&mut self.content, text_to_add);
         }
     }
     fn request_review(&mut self) {
-        if let Some(state) = self.state {
+        if let Some(state) = self.state.take() {
             self.state = Some(state.request_review());
         }
     }
     fn approve(&mut self) {
-        if let Some(state) = self.state {
+        if let Some(state) = self.state.take() {
             self.state = Some(state.approve());
         }
     }
@@ -87,25 +84,22 @@ trait State {
     fn content<'a>(&self, text: &'a str) -> &'a str {
         ""
     }
-    fn add_text(&mut self, text: String, text_to_add: &str) -> String {
-        text
+    fn add_text(&self, text: &mut String, text_to_add: &str) {
     }
-    fn request_review(self: Box<Self>) -> Box<State> {
-        self
-    }
-    fn approve(self: Box<Self>) -> Box<State> {
-        self
-    }
+    fn request_review(self: Box<Self>) -> Box<State>;
+    fn approve(self: Box<Self>) -> Box<State>;
 }
 
 struct Draft {}
 impl State for Draft {
-    fn add_text(&mut self, text: String, text_to_add: &str) -> String {
+    fn add_text(&self, text: &mut String, text_to_add: &str) {
         text.push_str(text_to_add);
-        text
     }
     fn request_review(self: Box<Self>) -> Box<State> {
         Box::new(Reviewing {})
+    }
+    fn approve(self: Box<Self>) -> Box<State> {
+        self
     }
 }
 
@@ -114,12 +108,21 @@ impl State for Reviewing {
     fn approve(self: Box<Self>) -> Box<State> {
         Box::new(Approved {})
     }
+    fn request_review(self: Box<Self>) -> Box<State> {
+        self
+    }
 }
 
 struct Approved {}
 impl State for Approved {
     fn content<'a>(&self, text: &'a str) -> &'a str {
         text
+    }
+    fn request_review(self: Box<Self>) -> Box<State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<State> {
+        self
     }
 }
 
