163bb27b8763393062ad365da63b8baed4f87e3d
GONI Guillaume <gdn3@live.fr>
2018-06-13 09:40:47 +0200

    fin adv types

---
+++ src/advanced_types.rs
@@ -0,0 +1,44 @@
+// ------------------------------
+// Advanced types
+pub fn run() {
+    // ------------------------------
+    // Type aliases
+    {
+        type Kilometers = i32;
+        type Thunk = Box<Fn() + Send + 'static>; // You wouldn't repeat that everywhere, would you ?
+        let f: Thunk = Box::new(|| println!("hi"));
+        fn takes_long_type(f: Thunk) { f(); }
+        takes_long_type(f);
+    }
+    // ------------------------------
+    // !, the type that never returns
+    {
+        // fn bar() -> ! {
+        // }
+
+        // It allows us to use like here a `continue` or `panic!` in a match (which requires
+        // returning the same type in all branches
+
+        // Here, continue has a value of !
+        // let guess: u32 = match guess.trim().parse() {
+        //         Ok(num) => num,
+        //         Err(_) => continue,
+        // };
+
+        // Panic! "is" a ! as well
+        // loop {} as well
+    }
+    // ------------------------------
+    // Dynamically sized types and `Sized`
+    {
+        // DST like str have to be stored in &str, Box<str>, Rc<str>
+        // Same for trait objects
+
+        // The trait `Sized` is given to every type that have a known size at compile time.
+        // Is is passed by default to generic parameters :
+        // fn generic<T>(t: T) {} == fn generic<T: Sized>(t: T) {}
+        //
+        // To disable that :
+        // fn generic<T: ?Sized>(t: &T) {}
+    }
+}
